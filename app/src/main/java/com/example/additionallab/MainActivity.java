ad73f1d31efa01eda6fcbdc5e7d986bed34518cd
malinya <it20211332@my.sliit.lk>
2021-08-22 11:10:14 +0530

    Created Media Player Start Button Stop Button

---
+++ app/src/main/java/com/example/additionallab/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.additionallab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            MediaPlayer player =
+                    MediaPlayer.create(this, Settings.System.DEFAULT_NOTIFICATION_URI);
+            player.setLooping(true);
+            player.start();
+
+        View btnStart = null;
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+        View btnStop = null;
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/additionallab/MyServices.java
@@ -0,0 +1,38 @@
+package com.example.additionallab;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+
+import androidx.annotation.Nullable;
+
+public class MyServices extends Service {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        MediaPlayer player = MediaPlayer.create(this,
+                Settings.System.DEFAULT_NOTIFICATION_URI);
+        player.setLooping(true);
+        player.start();
+        return START_STICKY;
+        //return super.onStartCommand(intent, flags, startId);
+    }
+
+    private MediaPlayer player;
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.stop();
+    }
+
+
+}
